b13806cdfe9f924f976f39fe994cdb8debed7781
themanjeetkr <themanjeetkr@gmail.com>
2025-03-22 23:11:43 +0530

    intro

---
+++ highorder/array/intro.js
@@ -0,0 +1,11 @@
+const student=["jhon","jharta","dolphin"]
+student[1]="macbook pro"
+console.log(student[1])
+
+
+//  pushing element at the back 
+student.push("alexa")
+student.push("siri")
+student.push("themanjeetkr")
+student.reverse()
+console.log(student)
--- highorder/usingarrow.js
+++ highorder/usingarrow.js
@@ -1,8 +1,14 @@
-function add( a,b,cb){
-    let result=a+b
-cb(result)
-}
+// function add( a,b,cb){
+//     let result=a+b
+// cb(result)
+// }
 
-// using arrow function 
-add(2,3,(val)=>console.log(val))
-add(4,5,(res)=>console.log(res))
+// // using arrow function 
+// add(2,3,(val)=>console.log(val))
+// add(4,5,(res)=>console.log(res))
+
+function multiply(a,b,cb){
+    let reult=a*b;
+    cb(reult);
+}
+multiply(2,3,val=>console.log(val))
