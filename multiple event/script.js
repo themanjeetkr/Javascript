d05e6413d536440e4e59cd7d66f0fc35cbb9db67
themanjeetkr <themanjeetkr@gmail.com>
2025-03-30 19:19:37 +0530

    somechanges

--- multiple event/script.js
+++ multiple event/script.js
@@ -1,11 +1,19 @@
 function alertpara(event){
-    alert("YOU HAVE CLICKED PAR" )
+    if(event.target.nodeName=='SPAN'){
+        alert("YOU HAVE CLICKED PARA" +event.target.textContent)
+    }
+   
 }
 
 
 
-let paras=document.querySelectorAll('p')
-for(let i=0;i<paras.length;i++){
-    let para=paras[i]
-    para.addEventListener('click',alertpara)
-}
+// let paras=document.querySelectorAll('p')
+// for(let i=0;i<paras.length;i++){
+//     let para=paras[i]
+//     para.addEventListener('click',alertpara)
+// // }
+// function message(){
+//     alert("")
+// }
+ let mydiv=document.getElementById('wrapper')
+ mydiv.addEventListener('click',alertpara)
