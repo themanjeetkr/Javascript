7aa9f696619e07ea19d2c8c18df3b9f675559704
themanjeetkr <themanjeetkr@gmail.com>
2025-03-30 12:17:56 +0530

    createelement

---
+++ throw.js
@@ -0,0 +1,14 @@
+// try{
+//     console.log(x)
+
+// }
+// catch(e){
+//     throw new Error(404 );
+    
+// }
+
+
+// 
+let errorcode=404;
+if(errorcode==404)
+    throw new Error('invalid json')
---
+++ try_catch.js
@@ -0,0 +1,17 @@
+try{
+    console.log('hello')
+    console.log(x)
+    console.log("always beign foreve")
+
+}
+catch(err){
+    console.log("error yaha hai")
+    console.log(err)
+}
+finally{
+    console.log("i will run everytime")
+}
+
+
+
+
